3e76b54a0d914dd821adf9712739d69e587b62fd
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-20 15:59:01 +0600

    Axios Complete

--- src/comopnents/axios/Axios.js
+++ src/comopnents/axios/Axios.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Axios = () => {
-  return <div>Axios</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resData = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setData(resData.data);
+    };
+
+    // Funciton Calling
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fetch API Using AXIOS</h1>
+
+      <br />
+      <br />
+      <br />
+      {/* Main Code */}
+      <div>
+        {data.map((singleData) => (
+          <ul key={singleData.id} style={{ listStyle: "none" }}>
+            <li>{singleData.userId}</li>
+            <li>{singleData.title}</li>
+            <li>{singleData.body}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Axios;
